3415a34cf41a4a56d2ac83d2ba9e38eb8a966522
Livânia Dantas <livaniavasconcelos@gmail.com>
2021-04-18 00:47:58 -0300

    exercicio_estrutura de controle

---
+++ Exercicio_estruturas_de_controle.R
@@ -0,0 +1,18 @@
+#Utilizando uma estrutura condicional para imprimir o resultado de um pleito eleitoral.
+
+candidato1 <-100
+candidato2 <- 28
+if (candidato1 > candidato2){
+  y <- ("candidato1_eleito")
+}else { 
+  y <- ("candidato2_eleito")}
+y
+
+
+candidato1 <-68
+candidato2 <- 70
+if (candidato1 > candidato2){
+  y <- ("candidato1_eleito")
+}else { 
+  y <- ("candidato2_eleito")}
+y
